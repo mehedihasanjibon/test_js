8f16f3b8161ec0d5c75ea611c982177fdab84c8b
Mehedi Hasan <mhjibon.info@gmail.com>
2022-12-12 15:43:42 +0600

    js statements

---
+++ js/java.js
@@ -0,0 +1,29 @@
+document.getElementById("demo").innerHTML = "Hello javascript";
+
+function myfunction(){
+    document.getElementById("demo1").style.fontSize = "45px";
+}
+
+function myfunction2(){
+    document.getElementById("demo2").style.display = "none";
+}
+
+function mybtn(){
+    document.getElementById("demo3").style.display = "block";
+}
+
+function mybtn2(){
+    document.getElementById("demo4").innerHTML = "paragraph change";
+}
+
+// js output
+document.write(5+5);
+
+// window.alert(5+6);
+
+console.log("Hello world");
+
+document.getElementById("demo5").innerHTML = "Hello Dolly";
+console.log(6);
+console.log(5); 
+document.write("Hello");
