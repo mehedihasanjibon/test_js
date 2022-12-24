010b4aee005a10565b78985089cb7a0e98b0ecee
Mehedi Hasan <mhjibon.info@gmail.com>
2022-12-24 22:28:15 +0600

    Find the Highest or lowest array value

--- js/java.js
+++ js/java.js
@@ -608,6 +608,18 @@ points2.sort(function(a ,b){
 });
 console.log(points2);
 
+// Find the Highest or lowest array value
+const points3 = [40, 100, 1, 5, 25, 10];
+points3.sort(function (a,b){
+    return a - b;
+});
+console.log(points3[0]);
+
+const points4 = [40, 100, 1, 5, 25, 10];
+points4.sort(function(a,b){
+    return b - a;
+});
+console.log(points4[0]);
 
 
 
