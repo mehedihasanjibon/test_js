d5f153b7da9de2e2dd0e73f092e57f3dc693aaaf
Mehedi Hasan <mhjibon.info@gmail.com>
2022-12-30 22:40:15 +0600

    java can hide the html element

--- js/java.js
+++ js/java.js
@@ -3,3 +3,7 @@ document.getElementById("demo").innerHTML = "Hello java";
 function myfunction(){
     document.getElementById("demo1").style.fontSize = "45px";
 }
+
+function myfunction2(){
+    document.getElementById("demo2").style.display = "none";
+}
