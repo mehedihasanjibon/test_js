76e467383deea92a0fd9dcc1808dd84fc899f56e
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-13 23:49:18 +0600

    show the display data

--- js/java.js
+++ js/java.js
@@ -4,8 +4,21 @@ const loadCountries = () => {
     .then(data => displayCountries(data));
 }
 
-const displayCountries = countries =>{
+const displayCountries = countries => {
     console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML);
+    const container = document.getElementById("countries");
+    container.innerHTML = countriesHTML;
 }
+
+const getCountryHTML = country => {
+    return `
+        <div class="country">
+            <h2>${country.name.common}</h2>
+        </div>
+    `
+}
+
 
 loadCountries(); 
